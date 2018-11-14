9d217ed5c27653804db88d911eea790629d742b3
Samed Güner <doktorgibsonyt@gmail.com>
2018-11-14 23:31:47 +0100

    Add L1 norm for screenshots
    - remove onPaint() counter for now

--- datacrawler/datacrawler-project/datacrawler-source/datamodules/screenshot-datamodule/ScreenshotHandler.cpp
+++ datacrawler/datacrawler-project/datacrawler-source/datamodules/screenshot-datamodule/ScreenshotHandler.cpp
@@ -2,7 +2,6 @@
 // Created by samed on 01.11.18.
 //
 
-#include <include/cef_app.h>
 #include "ScreenshotHandler.h"
 
 ScreenshotHandler::~ScreenshotHandler() {
@@ -12,6 +11,7 @@ ScreenshotHandler::~ScreenshotHandler() {
 ScreenshotHandler::ScreenshotHandler() {
     renderHeight = 600;
     renderWidth = 800;
+    deltaNorm = 1;
     logger = Logger::getInstance();
 
 }
@@ -20,6 +20,7 @@ ScreenshotHandler::ScreenshotHandler(NodeElement* nodeElement, int renderHeight,
     this->nodeElement = nodeElement;
     this->renderHeight = renderHeight;
     this->renderWidth = renderWidth;
+    deltaNorm = 1;
     logger = Logger::getInstance();
 
 }
@@ -33,12 +34,33 @@ void ScreenshotHandler::OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType 
         const void *buffer, int width, int height) {
 
 
-    milliseconds invokeTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
-    invokes.push_back(invokeTime.count());
+    //milliseconds invokeTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+    //invokes.push_back(invokeTime.count());
 
     logger->info("Painting!");
-    logger->info(std::to_string(invokes.front()));
-    
+    //logger->info(std::to_string(invokes.front()));
+    auto norm = calculateL1Norm(buffer, width, height);
+    deltaNorm = abs(lastL1Norm - norm);
+
+    logger->info("L1 distance between screenshots: "+std::to_string(deltaNorm));
+
     nodeElement = nullptr;
-    //CefQuitMessageLoop();
+
+   /* if(deltaNorm == 0)
+        CefQuitMessageLoop();
+    else */
+        lastL1Norm = norm;
+
+}
+
+ int32_t ScreenshotHandler::calculateL1Norm(const void* mat, int32_t  numCol, int32_t numRow) {
+    unsigned char * matrix = (unsigned char *) mat;
+    int32_t l1 = 0;
+
+     // our matrix is an array each pixel is represented by 4 * 2 Bytes due BRGA
+     // if we take a screenshot of 640*420 = 268800 pixels, we have to consider 1075200 pixels
+    for(int32_t i = 0; i < numRow*numCol * 4; i++ )
+            l1 +=  (int32_t) *(matrix + i);
+
+    return l1;
 }
--- datacrawler/datacrawler-project/datacrawler-source/datamodules/screenshot-datamodule/ScreenshotHandler.h
+++ datacrawler/datacrawler-project/datacrawler-source/datamodules/screenshot-datamodule/ScreenshotHandler.h
@@ -6,11 +6,14 @@
 #define DATACRAWLER_PROJECT_SCREENSHOTHANDLER_H
 
 #include <include/cef_render_handler.h>
+#include <include/cef_app.h>
+
 #include "../../util/Logger.h"
 #include "../../graph/NodeElement.h"
 
 #include <chrono>
 #include <list>
+#include <cmath>
 
 using namespace std::chrono;
 
@@ -21,13 +24,13 @@ private:
     int renderHeight;
     int renderWidth;
     NodeElement* nodeElement;
-
-    list<long> invokes;
-    int counter;
+    int32_t deltaNorm;
+    int32_t lastL1Norm;
 
 public:
     bool GetViewRect(CefRefPtr<CefBrowser> , CefRect &) OVERRIDE;
     void OnPaint(CefRefPtr<CefBrowser>, PaintElementType, const RectList &, const void*, int, int) OVERRIDE;
+    int32_t calculateL1Norm(const void*, int32_t , int32_t);
     
     ScreenshotHandler();
     ScreenshotHandler(NodeElement*, int, int);
