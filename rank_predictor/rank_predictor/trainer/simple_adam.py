852d5bcaba5aba2f32861f8e21403f6622ee0861
Timo Denk <github@timodenk.com>
2019-01-20 21:42:37 +0100

    Improve data loading

--- rank_predictor/rank_predictor/trainer/simple_adam.py
+++ rank_predictor/rank_predictor/trainer/simple_adam.py
@@ -1,39 +1,26 @@
+import logging
 import os
-
 import torch
-
 from rank_predictor.data.v1.pagerank_dataset import ScreenshotPagerankDatasetV1
-from rank_predictor.data.v1.transforms import ImageTransform, ToCudaTensor
 from rank_predictor.model.screenshot_feature_extractor import ScreenshotFeatureExtractor
 from torch import nn, optim
-from torch.utils.data import DataLoader
-from torchvision.transforms import Normalize, Resize, ToTensor, Compose, ToPILImage
-
 from rank_predictor.trainer.training_run import TrainingRun
 
+
+logging.basicConfig(level=logging.INFO)
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
-composed_transforms = Compose([
-    ImageTransform(ToPILImage()),
-    ImageTransform(Resize((1920//4, 1080//4))),
-    ImageTransform(ToCudaTensor(device)),
-    ImageTransform(Normalize((.5, .5, .5, .5), (.5, .5, .5, .5))),
-])
-
 # dataset
-pagerank_v1 = ScreenshotPagerankDatasetV1(
-    root_dir=os.getenv('dataset_v1_path'), transform=composed_transforms)
-
-# dataset loader
-dataloader = DataLoader(pagerank_v1, batch_size=4, shuffle=True)
+dataset_dir = os.getenv('dataset_v1_path')
+dataset_v1 = ScreenshotPagerankDatasetV1.get_threefold(dataset_dir, train_ratio=0.85, valid_ratio=0.1)
 
 # model with weights
 net = ScreenshotFeatureExtractor()
 net.cuda()
 
 # optimizer
-opt = optim.Adam(net.parameters(), lr=3e-5)
+opt = optim.Adam(net.parameters(), lr=1e-5)
 
-training_run = TrainingRun(net, opt, nn.MSELoss(), dataloader, device)
+training_run = TrainingRun(net, opt, nn.MSELoss(), dataset_v1, batch_size=24, device=device)
 training_run(1)
--- rank_predictor/rank_predictor/trainer/training_run.py
+++ rank_predictor/rank_predictor/trainer/training_run.py
@@ -1,42 +1,85 @@
+import logging
+import multiprocessing
+
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+from rank_predictor.data import threefold
 
 
 class TrainingRun:
     def __init__(self,
                  net: nn.Module,
-                 opt: optim.Optimizer,
+                 opt: optim.Adam,
                  loss_fn,
-                 data: DataLoader,
+                 data: threefold.Data,
+                 batch_size: int,
                  device) -> None:
         self.net = net
         self.opt = opt
         self.loss_fn = loss_fn
-        self.data = data
         self.step_ctr = 0
         self.device = device
+
+        cpu_count = multiprocessing.cpu_count()
+        worker_count = max(cpu_count - 1, 1)
+        logging.info("Using {} workers for the data pipeline".format(worker_count))
+
+        # create data loader from dataset
+        self.data: threefold.Data[DataLoader] = threefold.Data(
+            train=DataLoader(data.train, batch_size, shuffle=True, num_workers=worker_count),
+            valid=DataLoader(data.valid, batch_size, shuffle=False, num_workers=worker_count),
+            test=DataLoader(data.test, batch_size, shuffle=False, num_workers=worker_count),
+        )
 
         self.loss_log = []
 
         self.net.to(device)
 
+        self.writer = SummaryWriter('logs')
+
     def __call__(self, epochs: int) -> None:
         for epoch in range(epochs):
-            for batch in self.data:
+            logging.info("Starting epoch #{}".format(epoch + 1))
+            for batch in self.data.train:
+                if self.step_ctr % 5 == 0:
+                    self._run_valid()
+
                 imgs = batch['img'].to(self.device)
                 ranks = batch['rank'].to(self.device)
-                self._step(imgs, ranks)
+                self._train_step(imgs, ranks)
 
-    def _step(self, inputs: torch.Tensor, targets: torch.Tensor) -> None:
+    def _train_step(self, inputs: torch.Tensor, targets: torch.Tensor) -> None:
+        self.net.train()
+
         self.step_ctr += 1
         self.opt.zero_grad()
+
         model_out = self.net.forward(inputs)
+
         loss = self.loss_fn(model_out, targets.float() / 1e5)
         loss.backward()
-        self._log_loss(loss)
         self.opt.step()
 
-    def _log_loss(self, loss) -> None:
-        self.loss_log.append(loss.item())
-        print(self.loss_log)
+        self.writer.add_scalar('loss_train', loss, self.step_ctr)
+
+    def _run_valid(self) -> None:
+        logging.info("Running validation")
+
+        self.net.eval()
+
+        loss_sum = 0.
+
+        with torch.no_grad():
+            for batch in tqdm(self.data.valid):
+                imgs = batch['img'].to(self.device)
+                ranks = batch['rank'].to(self.device)
+                model_out = self.net.forward(imgs)
+
+                loss_sum += self.loss_fn(model_out, ranks.float() / 1e5)
+
+            loss = loss_sum / len(self.data.valid)
+
+        self.writer.add_scalar('loss_valid', loss, self.step_ctr)
