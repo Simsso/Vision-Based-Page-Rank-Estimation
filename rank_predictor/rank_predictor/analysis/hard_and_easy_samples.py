75b54f7c6d34f121718866b71bbeb74803f6a9b4
Timo Denk <github@timodenk.com>
2019-04-27 15:30:13 +0200

    Analyse accuracy vs. rank

--- rank_predictor/rank_predictor/analysis/hard_and_easy_samples.py
+++ rank_predictor/rank_predictor/analysis/hard_and_easy_samples.py
@@ -3,16 +3,17 @@ Post-training analysis script that finds hard and easy samples.
 """
 
 import torch
+from rank_predictor.data.v2.pagerank_dataset import DatasetV2
 from tqdm import tqdm
-from rank_predictor.analysis.utils import get_env_vars, restore_model, get_data
-from rank_predictor.model.graph_extractor_full import GraphExtractorFull
+from rank_predictor.analysis.utils import get_env_vars, restore_model, get_data, chunks, avg
 import numpy as np
+from rank_predictor.model.graph_only_models import GNAvg
 from rank_predictor.trainer.ranking.utils import per_sample_accuracy
 
 
 env = get_env_vars()
-model = restore_model(env, GraphExtractorFull, {'num_core_blocks': 1, 'drop_p': 0.})
-data = get_data(env)
+model = restore_model(env, GNAvg, {})
+data = get_data(env, train_ratio=.6, valid_ratio=.2, cached=True)
 
 r = []  # vector of ranks
 f = []  # model outputs vector
@@ -43,3 +44,22 @@ print(rank_acc_vec_rank)
 
 tqdm.write("Sorted by accuracy")
 print(rank_acc_vec_acc)
+
+
+batch_rank_span = 4000
+max_rank = DatasetV2.max_rank+1
+num_batches = max_rank//batch_rank_span
+batch_r = [[] for _ in range(num_batches)]
+batch_acc = [[] for _ in range(num_batches)]
+
+for r, acc in rank_acc_vec_rank:
+    batch = min(r // batch_rank_span, num_batches-1)
+    batch_r[batch].append(r)
+    batch_acc[batch].append(acc)
+
+tqdm.write("Accuracy vs. rank")
+for i in range(num_batches):
+    r_mean = np.mean(batch_r[i])
+    acc_mean = np.mean(batch_acc[i])
+    stddev = np.std(batch_acc[i])
+    tqdm.write("({}, {}) +- ({},{})".format(r_mean, acc_mean, stddev, stddev))
--- rank_predictor/rank_predictor/analysis/utils.py
+++ rank_predictor/rank_predictor/analysis/utils.py
@@ -1,10 +1,11 @@
 import os
-from typing import Dict, Callable
+from typing import Dict, Callable, Iterable
 
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from rank_predictor.data.v2.pagerank_dataset import DatasetV2
+from rank_predictor.data.v2.pagerank_dataset_cached import DatasetV2Cached
 
 
 def get_env_vars() -> Dict[str, str]:
@@ -18,9 +19,12 @@ def get_env_vars() -> Dict[str, str]:
     dataset_dir = os.path.expanduser(os.getenv('dataset_v2_path'))
     assert os.path.isdir(dataset_dir), "Environment variables 'dataset_v2_path' must point to a directory"
 
+    feat_extr_weights_path = os.path.expanduser(os.getenv('feat_extr_weights_path'))
+
     return {
         'model_weights_path': model_weights_path,
-        'dataset_dir': dataset_dir
+        'dataset_dir': dataset_dir,
+        'feat_extr_weights_path': feat_extr_weights_path
     }
 
 
@@ -34,8 +38,23 @@ def restore_model(env: Dict[str, str], model_class: Callable, kwargs: Dict[str, 
     return model
 
 
-def get_data(env: Dict[str, str], train_ratio: float = .85, valid_ratio: float = .1) -> DataLoader:
+def get_data(env: Dict[str, str], train_ratio: float = .85, valid_ratio: float = .1, cached: bool = False) -> DataLoader:
     tqdm.write("Loading dataset")
-    data = DatasetV2.get_threefold(env['dataset_dir'], train_ratio, valid_ratio, logrank_b=2)
-    data = DataLoader(data.test, 1, shuffle=False, num_workers=0, collate_fn=lambda b: b)
+    if not cached:
+        data = DatasetV2.get_threefold(env['dataset_dir'], train_ratio, valid_ratio, logrank_b=10)
+    else:
+        data = DatasetV2Cached.get_threefold(env['dataset_dir'], train_ratio, valid_ratio, logrank_b=10,
+                                             feat_extr_weights_path=env['feat_extr_weights_path'])
+    data = DataLoader(data.train, 1, shuffle=False, num_workers=0, collate_fn=lambda b: b)
     return data
+
+
+def chunks(l: list, n: int) -> Iterable[list]:
+    """yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def avg(l: list) -> float:
+    assert len(l) > 0, "List must not be empty"
+    return sum(l) / len(l)
