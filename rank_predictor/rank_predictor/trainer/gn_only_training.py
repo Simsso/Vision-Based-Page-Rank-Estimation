4804c62419e22506343fb5584c8aa28db61824f4
Timo Denk <github@timodenk.com>
2019-05-03 22:54:33 +0200

    Configure run '10wob_deep_01'

--- rank_predictor/rank_predictor/trainer/gn_only_training.py
+++ rank_predictor/rank_predictor/trainer/gn_only_training.py
@@ -10,7 +10,7 @@ from rank_predictor.trainer.ranking.probabilistic_loss import ProbabilisticLoss
 from sacred import Experiment
 from sacred.observers import MongoObserver
 
-name = '10wob_max_01'
+name = '10wob_deep_01'
 ex = Experiment(name)
 
 ex.observers.append(MongoObserver.create(url='mongodb://localhost:27017/sacred'))
@@ -24,12 +24,12 @@ def run_config():
     epochs = 10
     optimizer = 'adam'
     train_ratio, valid_ratio = .6, .2
-    model_name = 'GNMax'
+    model_name = 'GNDeep'
     loss = 'ProbabilisticLoss'
     weighting = 'c_ij = c_ij !*w & scaling_fac'
     logrank_b = 10
-    drop_p = 0
-    num_core_blocks = 0
+    drop_p = .1
+    num_core_blocks = 1
     share_core_weights = False
     lr_scheduler = 'None'
     lr_scheduler_gamma = None
